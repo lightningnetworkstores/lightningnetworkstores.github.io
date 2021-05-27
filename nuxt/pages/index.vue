8ffcd21270d6d31e4560f356f73b951997a6f444
Ed <a>
2021-05-27 23:24:44 +0200

    Add StoreCard component

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -70,141 +70,11 @@
       </v-layout>
 
       <v-container class="full-list">
-        <v-card
-          hover
+        <store-card
           v-for="store in getStores.slice(0, maxCards)"
           :key="'store-' + store.id"
-          @click.native="gotoStore(store.id)"
-        >
-          <v-card-text class="pa-0">
-            <div class="detail">
-              <div class="screenshot">
-                <v-img
-                  :aspect-ratio="4 / 3"
-                  max-height="200px"
-                  :src="`${baseURL}thumbnails/${store.id}.jpg`"
-                  class="text-right"
-                  @click.native="gotoStore(store.id)"
-                >
-                  <v-chip
-                    v-if="isNewStore(store)"
-                    color="green"
-                    text-color="white"
-                    class="ma-2"
-                    >New</v-chip
-                  >
-                  <v-chip
-                    v-if="store.trending >= 10"
-                    color="purple"
-                    text-color="white"
-                    class="ma-2"
-                    >Trending</v-chip
-                  >
-                  <v-chip
-                    v-if="store.rank !== 'unranked'"
-                    color="#fdb919"
-                    text-color="white"
-                    class="ma-2"
-                    >#{{ store.rank }}</v-chip
-                  >
-                </v-img>
-              </div>
-              <div class="score">
-                <vote-button :isUpvoting="true" :store="store" />
-                <span> {{ store.upvotes | splitNumber }}</span>
-                <vote-button :isUpvoting="false" :store="store" />
-              </div>
-              <div class="content pa-2 pl-5">
-                <div class="title">
-                  <a :href="store.href" class="font-weight-regular">
-                    {{ store.name }}
-                    <v-icon class="ml-1" color="blue darken-2"
-                      >mdi-open-in-new</v-icon
-                    >
-                  </a>
-                </div>
-                <div class="description">{{ store.description }}</div>
-                <div>
-                  <div class="tag-container">
-                    <!-- <v-chip
-                      v-for="(tag, index) in store.tags.slice(0, 5)"
-                      :key="index"
-                      color="primary"
-                      outlined
-                      small
-                      class="mr-2 my-1"
-                    >
-                      <b>{{ tag }}</b>
-                    </v-chip> -->
-
-                    <!-- <v-chip
-                      v-for="(tag, index) in store.tags.slice(0, 5)"
-                      :key="index"
-                      color="primary"
-                      outlined
-                      small
-                      class="mr-2 my-1"
-                    >
-                      <b>{{ tag }}</b>
-                    </v-chip> -->
-                    <v-chip
-                      v-if="store.tags[0]"
-                      color="primary"
-                      outlined
-                      small
-                      class="mr-2 my-1"
-                    >
-                      <b>{{ store.tags[0] }}</b>
-                    </v-chip>
-                    <v-menu
-                      open-on-hover
-                      top
-                      offset-y
-                      v-if="store.tags.length > 1"
-                    >
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-chip
-                          v-bind="attrs"
-                          v-on="on"
-                          color="primary"
-                          outlined
-                          small
-                          class="mr-2 my-1"
-                        >
-                          <b
-                            >+{{ store.tags.length - 1 }} tag{{
-                              store.tags.length - 1 == 1 ? '' : 's'
-                            }}</b
-                          >
-                        </v-chip>
-                      </template>
-
-                      <v-list>
-                        <v-list-item
-                          v-for="(tag, index) in store.tags.slice(1, 5)"
-                          :key="index"
-                          class="my-0"
-                        >
-                          <v-chip
-                            color="primary"
-                            outlined
-                            small
-                            class="mr-2 my-0"
-                          >
-                            <b>{{ tag }}</b>
-                          </v-chip>
-                        </v-list-item>
-                      </v-list>
-                    </v-menu>
-                  </div>
-                  <div class="comments" v-if="store.total_comments">
-                    <v-icon small>fa-comment</v-icon> {{ store.total_comments }}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </v-card-text>
-        </v-card>
+          :store="store"
+        ></store-card>
       </v-container>
       <v-container fill-height v-if="isLoading">
         <v-layout row justify-center align-center>
@@ -228,9 +98,9 @@
 
 <script>
 import AddStoreModal from '~/components/AddStoreModal.vue'
-import VoteButton from '../components/VoteButton.vue'
+import StoreCard from '../components/StoreCard.vue'
 export default {
-  components: { AddStoreModal, VoteButton },
+  components: { AddStoreModal, StoreCard },
   data() {
     return {
       drawer: false,
@@ -253,12 +123,6 @@ export default {
     }
   },
   methods: {
-    gotoStore(store_id) {
-      this.$router.push('/store/' + store_id)
-    },
-    isNewStore(store) {
-      return new Date(store.added * 1000 + 1000 * 60 * 60 * 24 * 8) > new Date()
-    },
     toggleDrawer() {
       this.drawer = !this.drawer
     },
