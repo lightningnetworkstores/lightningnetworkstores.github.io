fcfc1217a79f16821294da759d19a02204ab36bf
work <emeceve@outlook.com>
2021-06-22 13:08:37 -0300

    Fixes error page and customize it

--- nuxt/layouts/error.vue
+++ nuxt/layouts/error.vue
@@ -1,18 +1,19 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
-  </v-app>
+  <v-layout class="justify-center error-wrapper column align-center">
+    <div v-if="error.statusCode === 404">
+      <h1 class="error-code">404</h1>
+      <p class="error-message">{{ pageNotFound }}</p>
+    </div>
+    <div v-else>
+      <p class="error-message">{{ otherError }}</p>
+    </div>
+    <v-btn depressed to="/">Back to home page</v-btn>
+  </v-layout>
 </template>
 
 <script>
 export default {
-  layout: 'empty',
+  layout: 'default',
   props: {
     error: {
       type: Object,
@@ -21,8 +22,8 @@ export default {
   },
   data() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
+      pageNotFound: 'Page not found...',
+      otherError: 'An error occurred...',
     }
   },
   head() {
@@ -35,8 +36,19 @@ export default {
 }
 </script>
 
-<style scoped>
-h1 {
+<style scoped lang="scss">
+.error-wrapper {
+  height: 100%;
+}
+.error-code {
+  text-align: center;
+  font-size: 100px;
+  letter-spacing: 20px;
+  font-weight: 300;
+  color: rgb(253, 185, 25);
+}
+.error-message {
+  text-align: center;
   font-size: 20px;
 }
 </style>
