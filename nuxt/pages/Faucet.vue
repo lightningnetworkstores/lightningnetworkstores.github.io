d8472304cbda19146de4d10579e0352856628ca1
Engels Baca Sevilla <engelsbaca@spartansv.net>
2021-07-02 10:25:16 -0600

    Update layout and adding link to rows

--- nuxt/pages/Faucet.vue
+++ nuxt/pages/Faucet.vue
@@ -3,12 +3,19 @@
     <v-layout justify-center>
       <v-flex xs12 md12 lg10 xl8 class="pa-6">
         <v-container>
-          <v-layout row class="donor-head">
-            <h1>Lightning faucet</h1>
-            <v-btn depressed color="orange" @click="donorDialog = true"
-              >Donate</v-btn
-            >
-          </v-layout>
+          <v-row>
+            <v-col cols="12" md="4" offset-md="4">
+              <h1 class="text-center">Lightning faucet</h1>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-layout justify-end>
+                <v-btn depressed color="orange" @click="donorDialog = true"
+                  >Donate</v-btn
+                >
+              </v-layout>
+            </v-col>
+          </v-row>
+
           <v-layout row pt-3 justify-center class="datatable-layout">
             <v-data-table
               :headers="headers"
@@ -16,9 +23,23 @@
               :items-per-page="20"
               :hide-default-footer="true"
               class="elevation-1"
-            ></v-data-table>
+            >
+              <template v-slot:item="{ item }">
+                <tr
+                  :style="{ cursor: 'pointer' }"
+                  @click="handleDonorClick(item)"
+                >
+                  <td>{{ item.name }}</td>
+                  <td>{{ item.message }}</td>
+                  <td>{{ item.total_donated }}</td>
+                  <td>{{ item.sats_per_claim }}</td>
+                </tr>
+              </template>
+            </v-data-table>
           </v-layout>
-          <v-layout v-if="message"><h3>{{message}}</h3></v-layout>
+          <v-layout v-if="message"
+            ><h3>{{ message }}</h3></v-layout
+          >
           <v-layout row pt-3 justify-center>
             <vue-hcaptcha
               ref="invisibleHcaptcha"
@@ -28,7 +49,7 @@
               @verify="onVerify"
             />
             <v-btn depressed color="orange" @click="runCaptcha">
-              Get {{claimAmount}} sat
+              Get {{ claimAmount }} sat
             </v-btn>
           </v-layout>
         </v-container>
@@ -44,8 +65,10 @@
     </v-dialog>
     <v-dialog persistent v-model="showDialog" max-width="500">
       <v-card>
-        <v-card-title v-if="openClaim" class="text-h5">Get sats via LNURL</v-card-title>
-        <v-card-title v-else="openClaim" class="text-h5">You got sats!</v-card-title>
+        <v-card-title v-if="openClaim" class="text-h5"
+          >Get sats via LNURL</v-card-title
+        >
+        <v-card-title v-else class="text-h5">You got sats!</v-card-title>
 
         <v-card-text>
           <Checkout
@@ -54,10 +77,7 @@
             :paymentRequest="paymentRequest"
             @cancel="handleCancel"
           />
-          <Success
-            v-if="successfulClaim"
-            @cancel="handleCancel"
-          />
+          <Success v-if="successfulClaim" @cancel="handleCancel" />
         </v-card-text>
       </v-card>
     </v-dialog>
@@ -72,7 +92,7 @@ export default {
   components: {
     VueHcaptcha,
     Checkout,
-    Success
+    Success,
   },
   data: () => ({
     headers: [
@@ -89,23 +109,28 @@ export default {
     openClaim: false,
     successfulClaim: false,
     paymentRequest: '',
-    interval: null
+    interval: null,
   }),
   created() {},
-  mounted(){
-    this.$store.dispatch('getFaucetDonors').then(
-      (response) => {
-        this.topDonors = response.data.data.top_donors
-        this.topDonors.map(e=>e["sats_per_claim"]=Math.round(e['sats_per_claim']*100)/100)
-        this.topDonors.sort((d1,d2)=>d2["sats_per_claim"]-d1["sats_per_claim"])
+  mounted() {
+    this.$store.dispatch('getFaucetDonors').then((response) => {
+      this.topDonors = response.data.data.top_donors
+      this.topDonors.map(
+        (e) =>
+          (e['sats_per_claim'] = Math.round(e['sats_per_claim'] * 100) / 100)
+      )
+      this.topDonors.sort(
+        (d1, d2) => d2['sats_per_claim'] - d1['sats_per_claim']
+      )
 
-        this.claimAmount = response.data.data.claim
-        if(response.data.message) this.message = response.data.message
-      }
-    )  
+      this.claimAmount = response.data.data.claim
+      if (response.data.message) this.message = response.data.message
+    })
   },
   computed: {
-      showDialog(){return this.openClaim || this.successfulClaim}
+    showDialog() {
+      return this.openClaim || this.successfulClaim
+    },
   },
   methods: {
     onVerify(token, ekey) {
@@ -119,33 +144,39 @@ export default {
           console.log(resp.data.data['lnurl-withdraw'])
         })
     },
-    checkClaimMethod (claimID) {
+    checkClaimMethod(claimID) {
       this.interval = setInterval(() => {
-        this.$store.dispatch('checkClaimRequest', { id: claimID } ).then((response) => {
-          if (response.data.claim_status=='PAID') {
-            clearInterval(this.interval);
-            this.successfulClaim = true;
-            this.openClaim = false
+        this.$store.dispatch('checkClaimRequest', { id: claimID }).then(
+          (response) => {
+            if (response.data.claim_status == 'PAID') {
+              clearInterval(this.interval)
+              this.successfulClaim = true
+              this.openClaim = false
+            }
+          },
+          (error) => {
+            console.log(error)
           }
-          
-        }, (error) => {
-          console.log(error);
-        });
-      }, 5000);
+        )
+      }, 5000)
     },
     runCaptcha() {
       this.$refs.invisibleHcaptcha.execute()
     },
-    handleCancel(){
-        this.successfulClaim = false;
-        this.openClaim = false
-        this.paymentRequest = ''
-        if(this.interval!=null) clearInterval(this.interval)
+    handleCancel() {
+      this.successfulClaim = false
+      this.openClaim = false
+      this.paymentRequest = ''
+      if (this.interval != null) clearInterval(this.interval)
     },
-    closeDonationDialog(){
-      console.log('closing...')
-      this.donorDialog=false
-    }
+    handleDonorClick(item) {
+      if (item.url) {
+        location.href = item.url
+      }
+    },
+    closeDonationDialog() {
+      this.donorDialog = false
+    },
   },
 }
 </script>
