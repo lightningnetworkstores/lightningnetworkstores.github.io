a2849a5085fc45431445c748dd6375c5c147b282
pedro-roberto <pedro.fr.22@gmail.com>
2021-07-03 04:42:32 +0100

    Simplify URL check

--- nuxt/mixins/regex.js
+++ nuxt/mixins/regex.js
@@ -2,7 +2,8 @@ export default {
   methods: {
     isValidUrl(text) {
       const URL_REGEX = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/
-      return URL_REGEX.test(text)
+      const URL_REGEX_NO_PROTOCOL = /[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/
+      return URL_REGEX.test(text) || URL_REGEX_NO_PROTOCOL.test(text)
     }
   }
 }
