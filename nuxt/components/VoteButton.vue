7fffbfc77aebcccb6121ae3790cf2fb30e7d4ebf
pedro-roberto <pedro.fr.22@gmail.com>
2021-07-05 02:32:53 +0100

    Read maximum comment size from configuration

--- nuxt/components/VoteButton.vue
+++ nuxt/components/VoteButton.vue
@@ -95,7 +95,7 @@
                     v-if="!parentReview"
                     v-model="upvoteDialogForm.comment"
                     type="text"
-                    counter="200"
+                    :counter="this.$store.state.configuration.max_comment_size"
                     :label="
                       'Review (optional - minimum ' +
                       replyReviewFee +
@@ -104,8 +104,8 @@
                     rows="4"
                     :rules="[
                       (v) =>
-                        v.length <= 200 ||
-                        'Review has to be shorter than 200 characters',
+                        v.length <= this.$store.state.configuration.max_comment_size ||
+                        'Review has to be shorter than ' + this.$store.state.configuration.max_comment_size + ' characters',
                     ]"
                   ></v-textarea>
                 </v-flex>
@@ -122,13 +122,13 @@
                     v-if="parentReview && parentComment"
                     v-model="upvoteDialogForm.comment"
                     type="text"
-                    counter="200"
+                    :counter="this.$store.state.configuration.max_comment_size"
                     label="Reply"
                     rows="4"
                     :rules="[
                       (v) =>
-                        v.length <= 200 ||
-                        'Reply has to be shorter than 200 characters',
+                        v.length <= this.$store.state.configuration.max_comment_size ||
+                        'Reply has to be shorter than ' + this.$store.state.configuration.max_comment_size + ' characters',
                       (v) => !!v || 'Reply is required',
                     ]"
                   ></v-textarea>
