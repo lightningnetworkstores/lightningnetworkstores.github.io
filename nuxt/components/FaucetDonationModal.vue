286330ca118bb5c135e4b6b836afedd2f808715a
Nelson R. Perez <bilthon@gmail.com>
2021-07-16 19:43:01 -0500

    refactor: replaces simple success checkmark for an animated one

--- nuxt/components/FaucetDonationModal.vue
+++ nuxt/components/FaucetDonationModal.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="FaucetDonationModal">
-    <form
-      v-if="!pendingDonation && !donationPaid"
-      @submit.prevent="submit"
-      ref="donationForm"
-    >
-      <v-text-field
-        v-model="amount"
-        type="number"
-        label="Amount"
-        required
-        :rules="[
-          (a) =>
-            a >= minDonationAmount ||
-            `amount can't be less than ${minDonationAmount}`,
-        ]"
-      ></v-text-field>
-      <v-subheader>Distribution period (weeks)</v-subheader>
-      <v-slider
-        v-model="distributionPeriodWeeks"
-        min="1"
-        :max="maximumDonationTimeoutDays / 7"
-        :thumb-label="true"
-        step="1"
-        class="align-center"
-      ></v-slider>
+    <div class="FaucetDonationModal">
+        <form
+            v-if="!pendingDonation && !donationPaid"
+            @submit.prevent="submit"
+            ref="donationForm"
+        >
+            <v-text-field
+                v-model="amount"
+                type="number"
+                label="Amount"
+                required
+                :rules="[
+                    (a) =>
+                        a >= minDonationAmount ||
+                        `amount can't be less than ${minDonationAmount}`,
+                ]"
+            ></v-text-field>
+            <v-subheader>Distribution period (weeks)</v-subheader>
+            <v-slider
+                v-model="distributionPeriodWeeks"
+                min="1"
+                :max="maximumDonationTimeoutDays / 7"
+                :thumb-label="true"
+                step="1"
+                class="align-center"
+            ></v-slider>
 
-      <!-- <v-text-field
+            <!-- <v-text-field
 				v-model="distributionPeriodWeeks"
         type="number"
 				label="Distribute in the next days"
@@ -35,122 +35,134 @@
 				required
 				></v-text-field> -->
 
-      <v-text-field
-        v-model="name"
-        :label="nameInputLabel"
-        :required="isNameRequired"
-      ></v-text-field>
-      <v-text-field v-model="message" label="Message (optional)"></v-text-field>
-      <v-text-field
-        v-model="url"
-        type="url"
-        label="URL (optional)"
-      ></v-text-field>
-      <v-btn class="mr-4" type="submit">submit</v-btn>
-      <v-btn @click="cancel">Close</v-btn>
-    </form>
-    <Checkout
-      v-if="pendingDonation"
-      :satoshi="satoshi"
-      :paymentRequest="paymentRequest"
-      @cancel="cancel"
-    />
-    <Success v-if="donationPaid" @cancel="cancel" />
-  </div>
+            <v-text-field
+                v-model="name"
+                :label="nameInputLabel"
+                :required="isNameRequired"
+            ></v-text-field>
+            <v-text-field
+                v-model="message"
+                label="Message (optional)"
+            ></v-text-field>
+            <v-text-field
+                v-model="url"
+                type="url"
+                label="URL (optional)"
+            ></v-text-field>
+            <v-btn class="mr-4" type="submit">submit</v-btn>
+            <v-btn @click="cancel">Close</v-btn>
+        </form>
+        <Checkout
+            v-if="pendingDonation"
+            :satoshi="satoshi"
+            :paymentRequest="paymentRequest"
+            @cancel="cancel"
+        />
+        <Success v-if="donationPaid" @cancel="cancel" />
+    </div>
 </template>
 <script>
 import Checkout from '@/components/Checkout.vue'
 import Success from '@/components/Success.vue'
 
 export default {
-  name: 'FaucetDonationModal',
-  props: ['maximumDonationTimeoutDays', 'minDonationAmount'],
-  components: {
-    Checkout,
-    Success,
-  },
-
-  data: () => ({
-    amount: 1000,
-    distributionPeriodWeeks: null,
-    name: '',
-    message: '',
-    url: '',
-    pendingDonation: false,
-    donationPaid: false,
-    paymentRequest: '',
-    satoshi: '',
-    interval: null,
-  }),
-  computed: {
-    isNameRequired() {
-      return !!this.message || !!this.url
+    name: 'FaucetDonationModal',
+    props: ['maximumDonationTimeoutDays', 'minDonationAmount'],
+    components: {
+        Checkout,
+        Success,
     },
-    nameInputLabel() {
-      return this.isNameRequired ? 'Name' : 'Name (optional)'
-    },
-  },
-  mounted() {
-    this.amount = this.minDonationAmount
-  },
-  methods: {
-    async submit() {
-      const requestObj = {
-        timeout_days: this.distributionPeriodWeeks * 7,
-        amount: this.amount,
-      }
-    
-      let recaptchaToken = await this.$recaptcha.execute('faucet_donation')
 
-      if (this.name) {
-        requestObj['name'] = this.name
-      }
-      if (this.message) {
-        requestObj['message'] = this.message
-      }
-      if (this.url) {
-        requestObj['url'] = this.url
-      }
-
-      this.$store.dispatch('doFaucetDonation', { data: requestObj, recaptchaToken }).then(
-        (response) => {
-          let data = response.data
-          this.paymentRequest = data.invoice
-          this.satoshi = data.amount
-          this.pendingDonation = true
-          this.checkInvoice(data.id)
+    data: () => ({
+        amount: 1000,
+        distributionPeriodWeeks: null,
+        name: '',
+        message: '',
+        url: '',
+        pendingDonation: false,
+        donationPaid: false,
+        paymentRequest: '',
+        satoshi: '',
+        interval: null,
+    }),
+    computed: {
+        isNameRequired() {
+            return !!this.message || !!this.url
         },
-        (error) => {
-          console.log(error)
-        }
-      )
+        nameInputLabel() {
+            return this.isNameRequired ? 'Name' : 'Name (optional)'
+        },
     },
-
-    checkInvoice(invoiceId) {
-      this.interval = setInterval(() => {
-        this.$store.dispatch('verifyInvoiceRequest', { id: invoiceId }).then(
-          (response) => {
-            if (response.data.paid) {
-              clearInterval(this.interval)
-              this.donationPaid = true
-              this.pendingDonation = false
+    mounted() {
+        this.amount = this.minDonationAmount
+    },
+    methods: {
+        async submit() {
+            const requestObj = {
+                timeout_days: this.distributionPeriodWeeks * 7,
+                amount: this.amount,
             }
-          },
-          (error) => {
-            console.log(error)
-          }
-        )
-      }, 5000)
-    },
 
-    cancel() {
-      //this.amount = ''
-      //this.distributionPeriodWeeks = ''
-      //this.name = ''
-      //this.message = ''
-      //this.url = ''
-      this.$emit('closeDialog')
+            let recaptchaToken = await this.$recaptcha.execute(
+                'faucet_donation'
+            )
+
+            if (this.name) {
+                requestObj['name'] = this.name
+            }
+            if (this.message) {
+                requestObj['message'] = this.message
+            }
+            if (this.url) {
+                requestObj['url'] = this.url
+            }
+
+            this.$store
+                .dispatch('doFaucetDonation', {
+                    data: requestObj,
+                    recaptchaToken,
+                })
+                .then(
+                    (response) => {
+                        let data = response.data
+                        this.paymentRequest = data.invoice
+                        this.satoshi = data.amount
+                        this.pendingDonation = true
+                        this.checkInvoice(data.id)
+                    },
+                    (error) => {
+                        console.log(error)
+                    }
+                )
+        },
+
+        checkInvoice(invoiceId) {
+            this.interval = setInterval(() => {
+                this.$store
+                    .dispatch('verifyInvoiceRequest', { id: invoiceId })
+                    .then(
+                        (response) => {
+                            if (response.data.paid) {
+                                clearInterval(this.interval)
+                                this.donationPaid = true
+                                this.pendingDonation = false
+                            }
+                        },
+                        (error) => {
+                            console.log(error)
+                        }
+                    )
+            }, 5000)
+        },
+
+        cancel() {
+            //this.amount = ''
+            //this.distributionPeriodWeeks = ''
+            //this.name = ''
+            //this.message = ''
+            //this.url = ''
+            this.$emit('closeDialog')
+        },
     },
-  },
 }
 </script>
--- nuxt/components/Success.vue
+++ nuxt/components/Success.vue
@@ -1,63 +1,136 @@
 <template>
-  <v-card flat class="text-center">
-    <v-card-title class="headline">
-      <v-row class="py-2">
-        <v-flex>{{ confirm_title }}</v-flex>
-      </v-row>
-    </v-card-title>
-    <v-icon size="100" color="green" pa-5>fas fa-check-circle</v-icon>
+    <v-card flat class="text-center">
+        <v-card-title class="headline">
+            <v-row class="py-2">
+                <v-flex>{{ confirm_title }}</v-flex>
+            </v-row>
+        </v-card-title>
+        <svg
+            class="checkmark"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 52 52"
+        >
+            <circle
+                class="checkmark__circle"
+                cx="26"
+                cy="26"
+                r="25"
+                fill="none"
+            />
+            <path
+                class="checkmark__check"
+                fill="none"
+                d="M14.1 27.2l7.1 7.2 16.7-16.8"
+            />
+        </svg>
+        <blockquote class="twitter-tweet" v-if="tweet">
+            <a :href="'https://twitter.com/x/status/' + tweet"></a>
+        </blockquote>
+        <script
+            async
+            src="https://platform.twitter.com/widgets.js"
+            charset="utf-8"
+        ></script>
+        <v-row v-if="message" class="ma-2 pt-2">
+            <v-flex>
+                <h3>{{ message }}</h3>
+            </v-flex>
+        </v-row>
+        <v-row v-if="store && Object.keys(store).length > 1" class="ma-2 pt-2">
+            <v-flex
+                >Go to
+                <a :href="'/store/' + store.rooturl">{{
+                    store.name
+                }}</a></v-flex
+            >
+        </v-row>
 
-    <blockquote class="twitter-tweet" v-if="tweet">
-      <a :href="'https://twitter.com/x/status/' + tweet"></a>
-    </blockquote>
-    <script
-      async
-      src="https://platform.twitter.com/widgets.js"
-      charset="utf-8"
-    ></script>
-    <v-row v-if="message" class="ma-2 pt-2">
-      <v-flex>
-          <h3>{{message}}</h3>
-      </v-flex>
-    </v-row>
-    <v-row v-if="store && Object.keys(store).length>1" class="ma-2 pt-2">
-      <v-flex
-        >Go to <a :href="'/store/' + store.rooturl">{{ store.name }}</a></v-flex
-      >
-    </v-row>
+        <v-card-actions>
+            <v-spacer></v-spacer>
 
-    <v-card-actions>
-      <v-spacer></v-spacer>
-
-      <v-btn color="green darken-1" text @click="handleClose"> Close </v-btn>
-    </v-card-actions>
-  </v-card>
+            <v-btn color="green darken-1" text @click="handleClose">
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
 <script>
 export default {
-  name: 'Success',
-  props: {
-    tweet: {
-      type: String,
-      default: '',
+    name: 'Success',
+    props: {
+        tweet: {
+            type: String,
+            default: '',
+        },
+        confirm_title: {
+            type: String,
+            default: '',
+        },
+        store: {
+            type: Object,
+            default: () => {},
+        },
+        message: {
+            type: String,
+            default: '',
+        },
     },
-    confirm_title: {
-      type: String,
-      default: '',
+    methods: {
+        handleClose() {
+            this.$emit('cancel')
+        },
     },
-    store: {
-      type: Object,
-      default: () => {},
-    },
-    message: {
-        type: String,
-        default: ''
-    }
-  },
-  methods: {
-    handleClose() {
-      this.$emit('cancel')
-    },
-  },
 }
 </script>
+<style scoped>
+.checkmark__circle {
+    stroke-dasharray: 166;
+    stroke-dashoffset: 166;
+    stroke-width: 2;
+    stroke-miterlimit: 10;
+    stroke: #7ac142;
+    fill: none;
+    animation: stroke 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards;
+}
+
+.checkmark {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+    display: block;
+    stroke-width: 2;
+    stroke: #fff;
+    stroke-miterlimit: 10;
+    margin: 10% auto;
+    box-shadow: inset 0px 0px 0px #7ac142;
+    animation: fill 0.4s ease-in-out 0.4s forwards,
+        scale 0.3s ease-in-out 0.9s both;
+}
+
+.checkmark__check {
+    transform-origin: 50% 50%;
+    stroke-dasharray: 48;
+    stroke-dashoffset: 48;
+    animation: stroke 0.3s cubic-bezier(0.65, 0, 0.45, 1) 0.8s forwards;
+}
+
+@keyframes stroke {
+    100% {
+        stroke-dashoffset: 0;
+    }
+}
+@keyframes scale {
+    0%,
+    100% {
+        transform: none;
+    }
+    50% {
+        transform: scale3d(1.1, 1.1, 1);
+    }
+}
+@keyframes fill {
+    100% {
+        box-shadow: inset 0px 0px 0px 30px #7ac142;
+    }
+}
+</style>
