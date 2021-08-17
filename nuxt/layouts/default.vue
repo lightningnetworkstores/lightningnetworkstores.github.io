cf042502f9b4b8ab7322ce47811bcdfbb1c16048
Nelson Pérez <bilthon@gmail.com>
2021-08-17 16:23:02 -0500

    refactor: segregates SEO meta tag customizations to its own mixin

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -28,58 +28,18 @@
 </template>
 <script>
 import Header from '@/components/Header'
+import seo from '~/mixins/seo'
 import { mapState } from 'vuex'
 import AnnouncementModal from '@/components/AnnouncementModal'
 
 export default {
   components: { AnnouncementModal, Header },
-
+  mixins: [seo],
   async fetch() {
     await this.$store.dispatch('getAnnouncements')
   },
-
   data() {
     return {}
-  },
-  head() {
-    const { capitalize } = this.$options.filters
-    const customTitles = {
-      siteName: 'Lightning Network Stores',
-      fromTemplate: function (tag) {
-        return `${capitalize(tag)} tag | Lightning Network Stores`
-      },
-      fromMapping: function (tag) {
-        switch (tag.toLowerCase()) {
-          case 'wallet':
-            return `Wallets | ${this.siteName}`
-          // case 'clothes':
-          //   return `Clothes | ${siteNsme}`
-          default:
-            return this.fromTemplate(tag)
-        }
-      }
-    }
-    if(this.$route.query.tags) {
-      const tags = this.$route.query.tags.split(',')
-      if (tags.length === 1) {
-        const tag = tags[0]
-        return {
-          title: customTitles.fromMapping(tag),
-          meta: [
-            {
-              hid: 'og:title',
-              property: 'og:title',
-              content: customTitles.fromMapping(tag),
-            },
-            {
-              hid: 'twitter:title',
-              property: 'twitter:title',
-              content: customTitles.fromMapping(tag),
-            },
-          ]
-        }
-      }
-    }
   },
   beforeCreate() {
     if (this.$cookies.get('darkMode') !== undefined) {
