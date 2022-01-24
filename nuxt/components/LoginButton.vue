4eb748b02b87252c8865d73b8d26cd795aeb6d8d
Nelson Pérez <bilthon@gmail.com>
2022-01-23 20:13:09 -0500

    feature: introduces basic login button

---
+++ nuxt/components/LoginButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn text @click="handleLoginClick">
+    <v-icon>
+      mdi-account-circle
+    </v-icon>
+  </v-btn>
+</template>
+<script>
+export default {
+  methods: {
+    handleLoginClick() {
+      console.log('Clicked')
+    }
+  }
+}
+</script>
