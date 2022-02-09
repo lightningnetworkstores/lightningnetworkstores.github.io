c3481b6c5058213f2d9202b26dcf83dccc439d52
Nelson Pérez <bilthon@gmail.com>
2022-02-09 14:38:40 -0500

    feature: displays real list of transactions

--- nuxt/components/TxHistory.vue
+++ nuxt/components/TxHistory.vue
@@ -4,46 +4,77 @@
     :items="transfers"
     :items-per-page="15"
     class="elevation-2"  
-  />
+  >
+    <template v-slot:[`item.type`]="{ item }">
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            class="transfer-type"
+            v-bind="attrs"
+            v-on="on"
+            :color="getColor(item.type)"
+          >
+            {{ item.type === 'LN_WITHDRAW' ? 'mdi-arrow-up-bold-circle' : 'mdi-arrow-down-bold-circle' }}
+          </v-icon>
+        </template>
+        <span> {{ getTypeTooltip(item.type) }} </span>
+      </v-tooltip>
+    </template>
+  </v-data-table>
 </template>
 <script>
-const rawData = [
-        {
-          'amount': 10,
-          'receiver': 'TWITTER_2185073714',
-          'fee': 0,
-          'type': 'LN_DEPOSIT',
-          'timestamp': 1643598474534,
-          'status': 'DONE'
-        },
-        {
-          'amount': 200,
-          'receiver': 'TWITTER_2185073714',
-          'fee': 0,
-          'type': 'LN_DEPOSIT',
-          'timestamp': 1643598454747,
-          'status': 'EXPIRED'
-        }
-      ]
+import { mapState } from 'vuex'
+import { DateTime } from 'luxon'
+
 export default {
   data() {
     return {
       headers: [
-        { text: 'Amount', value: 'amount' },
         { text: 'Type', value: 'type' },
+        { text: 'Amount', value: 'amount' },
+        { text: 'Fee' , value: 'fee' },
         { text: 'Time', value: 'time' },
-        { text: 'From', value: 'from' },
-        { text: 'Fee' , value: 'fee' }
-
-      ],
-      transfers: rawData.map(item => ({
-        amount: item.amount,
-        type: item.type,
-        time: item.timestamp,
-        from: item.sender ? item.sender : '-',
-        fee: item.fee
-      }))
+        { text: 'From', value: 'from' }
+      ]
     }
-  }
+  },
+  methods: {
+    getColor(type) {
+      if (type === 'LN_WITHDRAW') {
+        return 'red'
+      } else if (type === 'LN_DEPOSIT') {
+        return 'green'
+      }
+    },
+    getTypeTooltip(type) {
+      switch(type) {
+        case 'LN_WITHDRAW':
+          return 'LN Withdrawal'
+        case 'LN_DEPOSIT':
+          return 'LN Deposit'
+        default:
+          return 'Unknown'
+      }
+    }
+  },
+  computed: mapState({
+    transfers: state => {
+      return state
+        .wallet.transfers
+        .filter(transfer => transfer.status === 'DONE')
+        .map(transfer => ({
+          amount: transfer.amount,
+          type: transfer.type,
+          time: DateTime.fromMillis(transfer.timestamp).toLocaleString(),
+          from: transfer.sender ? transfer.sender : '-',
+          fee: transfer.fee
+        }))
+    }
+  })
 }
 </script>
+<style scoped>
+.transfer-type {
+  cursor: pointer;
+}
+</style>
