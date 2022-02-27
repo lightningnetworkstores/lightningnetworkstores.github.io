24b0f527525fe27ca02060ee8fe33668f505a150
aguilarkevin <kevinaguilar0813@gmail.com>
2022-02-27 14:53:56 -0600

    wip: fix some bugs in PlaceBetModal

--- nuxt/components/PlaceBetModal.vue
+++ nuxt/components/PlaceBetModal.vue
@@ -2,7 +2,7 @@
   <v-dialog
     v-model="isOpen"
     @click:outside="$emit('update:isOpen', false)"
-    @keydown="$emit('update:isOpen', false)"
+    @keydown.esc="$emit('update:isOpen', false)"
     max-width="500px"
   >
     <v-card>
@@ -13,17 +13,13 @@
           v-model="amount"
           placeholder="20"
           hint="min 20 sats"
+          type="number"
           prefix="$"
           :rules="[rules.required, rules.number]"
         ></v-text-field>
       </v-card-text>
       <v-card-actions>
-        <v-btn
-          color="primary"
-          :disabled="disableDoneButton"
-          text
-          @click="placeBet"
-        >
+        <v-btn color="primary" :disabled="disableDoneButton" text @click="placeBet">
           Done
         </v-btn>
         <v-btn color="secondary" text @click="$emit('update:isOpen', false)">
@@ -36,42 +32,42 @@
 
 <script>
 export default {
-  props: ['option', 'contestId', 'isOpen'],
-  emits: ['update:isOpen'],
+  props: ["option", "contestId", "isOpen"],
+  emits: ["update:isOpen"],
   data() {
     return {
-      amount: '0',
+      amount: "0",
       rules: {
-        required: (value) => !!value || 'Required.',
-        counter: (value) => value.length <= 20 || 'Max 20 characters',
+        required: (value) => !!value || "Required.",
+        counter: (value) => value.length <= 20 || "Max 20 characters",
         number: (value) => {
-          const pattern = /^[0-9]*$/
-          return (pattern.test(value) && value >= 20) || 'Invalid amount.'
+          const pattern = /^[0-9]*$/;
+          return (pattern.test(value) && value >= 20) || "Invalid amount.";
         },
       },
-    }
+    };
   },
   computed: {
     disableDoneButton() {
-      return this.amount < 20 || this.amount.length < 1
+      return this.amount < 20 || this.amount.length < 1;
     },
   },
   methods: {
     placeBet() {
       this.$store
-        .dispatch('placeBet', {
+        .dispatch("placeBet", {
           contestID: this.contestId,
           choice: this.option,
           amount: this.amount,
         })
         .then(() => {
-          this.openAmountModal = false
-          this.amount = '0'
-          this.$store.dispatch('getQuizContest')
-        })
+          this.openAmountModal = false;
+          this.amount = "0";
+          this.$store.dispatch("getQuizContest");
+        });
     },
   },
-}
+};
 </script>
 
 <style></style>
