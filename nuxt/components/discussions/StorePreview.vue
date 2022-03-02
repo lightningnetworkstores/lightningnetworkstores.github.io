958ce8df47cda4e09924e5bf57ec6ad89afdeb84
Nelson Pérez <bilthon@gmail.com>
2022-03-02 11:47:08 -0500

    refactor: prevents overflow of text in the StorePreview component

--- nuxt/components/discussions/StorePreview.vue
+++ nuxt/components/discussions/StorePreview.vue
@@ -11,7 +11,7 @@
         <div class="text-subtitle-2 mx-2 my-1">
           <a :href="`${store.href}`" open="_blank" rel="noreferrer noopener">{{ store.name }}</a>
         </div>
-        <div class="text-body-2 mx-2 my-1">
+        <div class="text-body-2 mx-2 my-0" style="max-height: 60%; overflow: hidden">
           {{ store.description }}
         </div>
       </div>
