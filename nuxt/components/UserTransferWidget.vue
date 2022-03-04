788ede85d4c3d047b3df287b66043a502e9b8232
Pedro FR <pedro.fr.22@gmail.com>
2022-03-04 17:23:07 +0100

    Fix user nullpointer

--- nuxt/components/UserTransferWidget.vue
+++ nuxt/components/UserTransferWidget.vue
@@ -6,7 +6,7 @@
                 style="min-width: 8em"
             >
                 <v-avatar class="mr-1">
-                    <v-img v-if="user.image" :src="user.image" />
+                    <v-img v-if="user && user.image" :src="user.image" />
                     <v-icon large v-else>mdi-account-circle</v-icon>
                 </v-avatar>
                 <div v-if="user.handle">
@@ -26,12 +26,16 @@
                     </div>
                 </div>
                 <div v-else>
-                    <div v-if="user.name" class="text-caption py-0 my-0">
+                    <div
+                        v-if="user && user.name"
+                        class="text-caption py-0 my-0"
+                    >
                         {{ user.name }}
                     </div>
-                    <div v-else class="text-caption py-0 my-0">
+                    <div v-if="user" class="text-caption py-0 my-0">
                         {{ user.id }}
                     </div>
+                    <div v-else class="text-caption py-0 my-0">External</div>
                 </div>
             </div>
         </v-chip>
