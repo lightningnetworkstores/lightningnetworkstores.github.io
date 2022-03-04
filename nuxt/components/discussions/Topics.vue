5320674058f6e31118844bfe1828d8b10a365ab7
Pedro FR <pedro.fr.22@gmail.com>
2022-03-04 22:24:54 +0100

    Topic chip shows count and sorted by frequency

--- nuxt/components/discussions/Topics.vue
+++ nuxt/components/discussions/Topics.vue
@@ -6,8 +6,8 @@
     class="justify-content-center"
   >
     <div class="d-flex justify-center" style="width: 100%">
-      <v-chip filter outlined v-for="topic in topics" :key="topic">
-        {{ topic }}
+      <v-chip filter outlined v-for="topic in this.sortedTopics" :key="topic">
+        {{ topic.charAt(0).toUpperCase() + topic.slice(1).toLowerCase().replace('_', ' ') }}&nbsp;({{getTopicCount(topic)}})
       </v-chip>
     </div>
   </v-chip-group>
@@ -17,15 +17,25 @@ import { mapState } from 'vuex'
 export default {
   methods: {
     handleChange(index) {
-      const payload = { topic: this.topics[index], index: index }
+      const payload = { topic: this.sortedTopics[index], index: index }
       this.$emit('on-topic-selected', payload)
+    },
+    getTopicCount(t) {
+      if(t==='ALL') return this.$store.state.discussions.lastDiscussions.length
+      let disc = JSON.parse(JSON.stringify(this.$store.state.discussions.lastDiscussions))
+      return disc.filter(d=>d[0].topic===t).length
     }
   },
-  computed: mapState({
-    topics: state => [
-      'ALL',
-      ...state.discussions.topics
-    ]
-  })
+  computed: {
+    topics: function() {
+      return ['ALL', ...this.$store.state.discussions.topics]
+    },
+     sortedTopics: function() {
+      let ts = ['ALL', ...this.$store.state.discussions.topics]
+      ts.sort((t1,t2)=>(this.getTopicCount(t2)-this.getTopicCount(t1)))
+      console.log(ts)
+      return ts
+    }
+  }
 }
 </script>
