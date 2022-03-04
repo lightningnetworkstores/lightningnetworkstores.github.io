fbc99e8d799a776ded9c2222c3602a674bf5c688
Nelson Pérez <bilthon@gmail.com>
2022-03-04 11:40:32 -0500

    refactor: indentation changes

--- nuxt/components/TxHistory.vue
+++ nuxt/components/TxHistory.vue
@@ -1,44 +1,44 @@
 <template>
-    <v-data-table
-        :headers="headers"
-        :items="transfers"
-        :items-per-page="15"
-        class="elevation-2"
-    >
-        <template v-slot:[`item.amount`]="{ item }">
-            <div>
-                {{ formatAmount(item) }}
-            </div>
+  <v-data-table
+    :headers="headers"
+    :items="transfers"
+    :items-per-page="15"
+    class="elevation-2"
+  >
+    <template v-slot:[`item.amount`]="{ item }">
+      <div>
+        {{ formatAmount(item) }}
+      </div>
+    </template>
+    <template v-slot:[`item.type`]="{ item }">
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            class="transfer-type"
+            v-bind="attrs"
+            v-on="on"
+            :color="getColor(item)"
+          >
+            {{ getIcon(item.type) }}
+          </v-icon>
         </template>
-        <template v-slot:[`item.type`]="{ item }">
-            <v-tooltip bottom>
-                <template v-slot:activator="{ on, attrs }">
-                    <v-icon
-                        class="transfer-type"
-                        v-bind="attrs"
-                        v-on="on"
-                        :color="getColor(item)"
-                    >
-                        {{ getIcon(item.type) }}
-                    </v-icon>
-                </template>
-                <span> {{ getTypeTooltip(item.type) }} </span>
-            </v-tooltip>
+        <span> {{ getTypeTooltip(item.type) }} </span>
+      </v-tooltip>
+    </template>
+    <template v-slot:[`item.time`]="{ item }">
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <span v-bind="attrs" v-on="on"
+            >{{ getTimeAgo(item.time) }}
+          </span>
         </template>
-        <template v-slot:[`item.time`]="{ item }">
-            <v-tooltip bottom>
-                <template v-slot:activator="{ on, attrs }">
-                    <span v-bind="attrs" v-on="on"
-                        >{{ getTimeAgo(item.time) }}
-                    </span>
-                </template>
-                <span> {{ getFormattedTime(item.time) }}</span>
-            </v-tooltip>
-        </template>
-        <template v-slot:[`item.from`]="{ item }">
-            <UserTransferWidget :user="item.from" />
-        </template>
-    </v-data-table>
+        <span> {{ getFormattedTime(item.time) }}</span>
+      </v-tooltip>
+    </template>
+    <template v-slot:[`item.from`]="{ item }">
+      <UserTransferWidget :user="item.from" />
+    </template>
+  </v-data-table>
 </template>
 <script>
 import { mapState } from 'vuex'
@@ -47,110 +47,110 @@ import { format } from 'timeago.js'
 import UserTransferWidget from './UserTransferWidget'
 
 export default {
-    components: {
-        UserTransferWidget,
+  components: {
+    UserTransferWidget,
+  },
+  data() {
+    return {
+      headers: [
+        { text: 'Type', value: 'type' },
+        { text: 'Amount', value: 'amount' },
+        { text: 'Fee', value: 'fee' },
+        { text: 'Time', value: 'time' },
+        { text: 'To/From', value: 'from' },
+      ],
+    }
+  },
+  methods: {
+    formatAmount(item) {
+      if (item.receiver) {
+        return item.receiver.id === this.twitterID
+          ? `+${item.amount}`
+          : `-${item.amount}`
+      } else if (item.sender) {
+        return item.sender.id === this.twitterID
+          ? `-${item.amount}`
+          : `+${item.amount}`
+      }
+      return item.amount
     },
-    data() {
-        return {
-            headers: [
-                { text: 'Type', value: 'type' },
-                { text: 'Amount', value: 'amount' },
-                { text: 'Fee', value: 'fee' },
-                { text: 'Time', value: 'time' },
-                { text: 'To/From', value: 'from' },
-            ],
-        }
+    getIcon(type) {
+      switch (type) {
+        case 'LN_WITHDRAW':
+          return 'mdi-arrow-up'
+        case 'LN_DEPOSIT':
+          return 'mdi-arrow-down'
+        case 'TRANSFER':
+          return 'mdi-transfer'
+        case 'AFFILIATE_PAYOUT':
+          return 'mdi-account-multiple'
+        default:
+          return type
+      }
     },
-    methods: {
-        formatAmount(item) {
-            if (item.receiver) {
-                return item.receiver.id === this.twitterID
-                    ? `+${item.amount}`
-                    : `-${item.amount}`
-            } else if (item.sender) {
-                return item.sender.id === this.twitterID
-                    ? `-${item.amount}`
-                    : `+${item.amount}`
-            }
-            return item.amount
-        },
-        getIcon(type) {
-            switch (type) {
-                case 'LN_WITHDRAW':
-                    return 'mdi-arrow-up'
-                case 'LN_DEPOSIT':
-                    return 'mdi-arrow-down'
-                case 'TRANSFER':
-                    return 'mdi-transfer'
-                case 'AFFILIATE_PAYOUT':
-                    return 'mdi-account-multiple'
-                default:
-                    return type
-            }
-        },
-        getColor(item) {
-            if (item.type === 'LN_WITHDRAW') {
-                return 'red'
-            } else if (item.type === 'LN_DEPOSIT') {
-                return 'green'
-            } else if (item.type === 'TRANSFER') {
-                if (item.from.id === this.twitterID) return 'red'
-                else return 'green'
-            }
-        },
-        getTypeTooltip(type) {
-            switch (type) {
-                case 'LN_WITHDRAW':
-                    return 'LN Withdrawal'
-                case 'LN_DEPOSIT':
-                    return 'LN Deposit'
-                case 'TRANSFER':
-                    return 'Internal Transfer'
-                case 'AFFILIATE_PAYOUT':
-                    return 'Affiliate payout'
-                default:
-                    return type
-            }
-        },
-        getTimeAgo(timestamp) {
-            return format(timestamp)
-        },
-        getFormattedTime(timestamp) {
-            return DateTime.fromMillis(timestamp).toRFC2822()
-        },
+    getColor(item) {
+      if (item.type === 'LN_WITHDRAW') {
+        return 'red'
+      } else if (item.type === 'LN_DEPOSIT') {
+        return 'green'
+      } else if (item.type === 'TRANSFER') {
+        if (item.from.id === this.twitterID) return 'red'
+        else return 'green'
+      }
     },
-    computed: mapState({
-        twitterID: (state) => {
-            return state.wallet.profile.twitterID
-        },
-        transfers: (state) => {
-            return state.wallet.transfers
-                .filter((transfer) => transfer.status === 'DONE')
-                .map((transfer) => {
-                    const { twitterID } = state.wallet.profile
-                    let from = transfer.sender
-                    if (transfer.type === 'TRANSFER') {
-                        from =
-                            transfer.sender.id === twitterID
-                                ? transfer.receiver
-                                : transfer.sender
-                    }
-                    return {
-                        amount: transfer.amount,
-                        type: transfer.type,
-                        time: transfer.timestamp,
-                        from: from,
-                        receiver: transfer.receiver,
-                        sender: transfer.sender,
-                        fee: transfer.fee,
-                    }
-                })
-        },
-    }),
+    getTypeTooltip(type) {
+      switch (type) {
+        case 'LN_WITHDRAW':
+          return 'LN Withdrawal'
+        case 'LN_DEPOSIT':
+          return 'LN Deposit'
+        case 'TRANSFER':
+          return 'Internal Transfer'
+        case 'AFFILIATE_PAYOUT':
+          return 'Affiliate payout'
+        default:
+          return type
+      }
+    },
+    getTimeAgo(timestamp) {
+      return format(timestamp)
+    },
+    getFormattedTime(timestamp) {
+      return DateTime.fromMillis(timestamp).toRFC2822()
+    },
+  },
+  computed: mapState({
+    twitterID: (state) => {
+      return state.wallet.profile.twitterID
+    },
+    transfers: (state) => {
+      return state.wallet.transfers
+        .filter((transfer) => transfer.status === 'DONE')
+        .map((transfer) => {
+          const { twitterID } = state.wallet.profile
+          let from = transfer.sender
+          if (transfer.type === 'TRANSFER') {
+            from =
+              transfer.sender.id === twitterID
+                ? transfer.receiver
+                : transfer.sender
+          }
+          return {
+            amount: transfer.amount,
+            type: transfer.type,
+            time: transfer.timestamp,
+            from: from,
+            receiver: transfer.receiver,
+            sender: transfer.sender,
+            fee: transfer.fee,
+          }
+        })
+    },
+  }),
 }
 </script>
 <style scoped>
 .transfer-type {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
