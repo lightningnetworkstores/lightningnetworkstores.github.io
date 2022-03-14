d47eadbcf4c78682f0193f814f2b77db12ef1587
Pedro FR <pedro.fr.22@gmail.com>
2022-03-14 16:27:11 +0100

    Handle affiliate codes as path parameters. TODO: not give 404!

--- nuxt/middleware/affiliate.js
+++ nuxt/middleware/affiliate.js
@@ -1,6 +1,15 @@
 export default function ({ route, $axios }) {
+  const regex = new RegExp('af\/[a-zA-Z0-9]+');
   if (route.query.af) {
     $axios.post(`/api/affiliatereferral?af=${route.query.af}`)
+      .then(res => { })
+      .catch(err => {
+        console.error('Error while reporting affiliate code. err: ', err)
+      })
+  } else if (regex.test(route.path)) {
+    const pathComponents = route.path.split('/')
+    const affiliateCode = pathComponents[pathComponents.length - 1]
+    $axios.post('/api/affiliatereferral?af=' + affiliateCode)
       .then(res => { })
       .catch(err => {
         console.error('Error while reporting affiliate code. err: ', err)
