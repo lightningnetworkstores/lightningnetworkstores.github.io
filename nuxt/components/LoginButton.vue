aa9643d3fb3c59f3ad2531742d02c5aa69d2c7a2
erwin261996 <erwinmayorga26@gmail.com>
2022-03-17 15:35:51 -0600

    Add modal action for login

--- nuxt/components/LoginButton.vue
+++ nuxt/components/LoginButton.vue
@@ -1,24 +1,45 @@
 <template>
-  <v-btn text @click="handleLoginClick">
-    <v-avatar size="36" color="primary">
-      <v-icon dark>
-        mdi-account-circle
-      </v-icon>
-    </v-avatar>
-  </v-btn>
+    <div>
+        <v-dialog v-model="dialog" max-width="300">
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn text v-bind="attrs" v-on="on" class="mt-4">
+                    <v-avatar size="36" color="primary">
+                        <v-icon dark> mdi-account-circle </v-icon>
+                    </v-avatar>
+                </v-btn>
+            </template>
+            <v-card class="d-flex flex-column justify-center align-center">
+                <v-card-title class="text-h5"> Login </v-card-title>
+                <v-card-text>
+                    <v-btn color="primary" dark block @click="handleLoginClick">
+                        <v-icon color="white darken-2" class="mr-3">
+                            mdi-twitter
+                        </v-icon>
+                        LOGIN WITH TWITTER
+                    </v-btn>
+                </v-card-text>
+            </v-card>
+        </v-dialog>
+    </div>
 </template>
 <script>
 export default {
-  methods: {
-    handleLoginClick() {
-      this.$axios.get('/api/oauthlogin?platform=twitter')
-        .then(res => res.data)
-        .then(data => {
-          console.log(data)
-          const { request_token, authorization_url, platform } = data.data
-          window.location.replace(authorization_url)
-        })
-    }
-  }
+    data() {
+        return {
+            dialog: false,
+        }
+    },
+    methods: {
+        handleLoginClick() {
+            this.$axios
+                .get('/api/oauthlogin?platform=twitter')
+                .then((res) => res.data)
+                .then((data) => {
+                    const { request_token, authorization_url, platform } =
+                        data.data
+                    window.location.replace(authorization_url)
+                })
+        },
+    },
 }
 </script>
