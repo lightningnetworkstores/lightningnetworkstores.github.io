c787d09bd22623bd97bab9592a73bd0e6719806a
Nelson Pérez <bilthon@gmail.com>
2022-05-04 23:22:43 -0500

    feature: introduces the NetworkErrorSnackbar, meant to display error messages coming from the server

--- nuxt/utils/constants.js
+++ nuxt/utils/constants.js
@@ -1,3 +1,6 @@
 // Image upload constants
 export const IMAGE_TYPE_FILE_UPLOAD = 0
 export const IMAGE_TYPE_URL = 1
+
+// Error default timeout (in milliseconds)
+export const DEFAULT_ERROR_SNACKBAR_TIMEOUT = 3e3
