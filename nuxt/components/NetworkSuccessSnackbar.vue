26e853dad339728be52ba0dd9670738e0cff322b
Nelson Pérez <bilthon@gmail.com>
2022-12-02 12:16:59 -0500

    feature: introduces the NetworkSuccessSnackbar component and extends the 'network' vuex module to also hold success responses

---
+++ nuxt/components/NetworkSuccessSnackbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-snackbar v-model="showSnackbar">
+    {{ message }}
+    <template v-slot:action="{ attrs }">
+      <v-btn
+        color="green"
+        text
+        v-bind="attrs"
+        @click="showSnackbar = false"
+      >
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      showSnackbar: true
+    }
+  },
+  props: {
+    message: {
+      type: String,
+      default: 'Success!'
+    }
+  }
+}
+</script>
--- nuxt/store/review.js
+++ nuxt/store/review.js
@@ -3,14 +3,16 @@ export const state = () => ({
 })
 
 export const actions = {
-  postReview({ commit }, { stars, comment, storeID }) {
-    commit('setIsPosting', true)
+  postReview(context, { stars, comment, storeID }) {
+    const { commit, dispatch } = context
     const body = { storeID, comment, stars }
+    commit('setIsPosting', true)
     return this.$axios.post('/api/review', body)
-      .then(data => {
-        console.log('data: ', data)
+      .then(resp => dispatch('network/showResponse', resp, { root: true }))
+      .catch(err => {
+        console.error('Error while posting review. err: ', err)
+        dispatch('network/showError', err, { root: true})
       })
-      .catch(err => console.error('Error while posting review. err: ', err))
       .finally(() => commit('setIsPosting', false))
   }
 }
