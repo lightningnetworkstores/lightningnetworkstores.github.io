6b7f2dd170d8775c38ffb488d698b6f69da37302
Pedro FR <pedro.fr.22@gmail.com>
2022-12-06 04:11:56 +0000

    Add missing units.

--- nuxt/components/AffiliateRules.vue
+++ nuxt/components/AffiliateRules.vue
@@ -2,7 +2,7 @@
     <v-card>
         <v-card-title> Affiliate Program </v-card-title>
         <v-card-text v-if="affiliate">
-            You will currently earn {{ Math.round(affiliate.reward_per_referral*10)/10 }} for
+            You will currently earn {{ Math.round(affiliate.reward_per_referral*10)/10 }} sats for
             every unique user you refer to our website. Your affiliate code is
             <strong @click="copyAffiliate" class="affiliate-code">{{
                 affiliate.affiliate_code
