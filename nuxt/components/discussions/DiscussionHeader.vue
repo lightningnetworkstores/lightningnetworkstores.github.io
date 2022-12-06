385444feacb32938ef49ee1b0a369f9e15eaca5b
Nelson Pérez <bilthon@gmail.com>
2022-12-05 21:23:47 -0500

    feature: adds component displaying an image that when clicked shows the same image without cropping in a dialog

--- nuxt/components/discussions/DiscussionHeader.vue
+++ nuxt/components/discussions/DiscussionHeader.vue
@@ -48,19 +48,7 @@
       <v-col :cols="isMobile ? 12 : ((discussionHeader.link)? 12 : 7)" :class="{'pa-0': isMobile}">
         <v-row v-if="!isMobile" no-gutters>
           <v-col md="2" class="mr-3">
-             <v-img
-                v-if="discussionHeader.link"
-                width="100%"
-                max-height="100%"
-                max-width="100%"
-                aspect-ratio="1"
-                position="center center"
-                :lazy-src="discussionHeader.link"
-                :src="discussionHeader.link"
-                :style="{
-                  borderRadius: '5px',
-                }"
-              ></v-img>
+            <discussion-image :url="discussionHeader.link"/>
           </v-col>
           <v-col :md="((discussionHeader.link)? 9 : 12)">
             <div class="text-h6 font-weight-bold comment-title" style="flex-grow: 1"
@@ -127,12 +115,13 @@ import DiscussionReplyModal from './DiscussionReplyModal.vue'
 import DeleteCommentModal from './DeleteCommentModal'
 import ChangeTopicModal from '@/components/discussions/ChangeTopicModal'
 import UserComment from '@/components/discussions/UserComment'
+import DiscussionImage from '@/components/discussions/DiscussionImage'
 
 import { mapState } from 'vuex'
 
 export default {
   components: {
-    StorePreview, UserTag, DiscussionReplyModal, DeleteCommentModal, ChangeTopicModal, UserComment
+    StorePreview, UserTag, DiscussionReplyModal, DeleteCommentModal, ChangeTopicModal, UserComment, DiscussionImage
   },
   mixins: [ DateFromatter ],
   props: {
