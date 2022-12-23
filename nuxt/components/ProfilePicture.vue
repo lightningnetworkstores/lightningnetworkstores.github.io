ea8c6e1f13933499f09e53de09405c31015658b1
Nelson Pérez <bilthon@gmail.com>
2022-12-22 23:56:11 -0300

    feature: adds balance display layout

--- nuxt/components/ProfilePicture.vue
+++ nuxt/components/ProfilePicture.vue
@@ -1,8 +1,13 @@
 <template>
   <v-btn text v-bind="attrs" v-on="on">
-    <v-avatar size="36">
-      <img :src="src" alt="Profile Picture">
-    </v-avatar>
+    <div class="d-flex flex-column justify-center align-center">
+      <v-avatar size="36">
+        <img :src="src" alt="Profile Picture">
+      </v-avatar>
+      <v-chip x-small :ripple="false" class="profile-balance-container mx-6">
+        <div class="text-caption"> 21e6 <i class="fak fa-regular"></i></div>
+      </v-chip>
+    </div>
   </v-btn>
 </template>
 <script>
@@ -17,3 +22,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+.profile-balance-container {
+  margin-top: -5px;
+}
+</style>
