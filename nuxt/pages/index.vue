cbae0bb1f54f89647c09cf06fd25405f86b0d442
Pedro FR <pedro.fr.22@gmail.com>
2023-02-02 15:22:32 +0100

    Fix prev commit - is it this time?

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -453,7 +453,7 @@
           route
         )
 
-        console.log('total server time=' + Date.now() - this.serverStartTime)
+        console.log('total server time=' + Date.now() - store.serverStartTime)
         return { safeMode, selectedSort, searchQuery }
 
       } catch (err) {
