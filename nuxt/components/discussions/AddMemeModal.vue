e992465b2e53ed6c96812d96f35bcc1ed36b173f
Nelson Pérez <bilthon@gmail.com>
2023-02-03 20:37:36 -0500

    feature: allows the admin to specify a creation date for a meme, the default being now

--- nuxt/components/discussions/AddMemeModal.vue
+++ nuxt/components/discussions/AddMemeModal.vue
@@ -1,13 +1,19 @@
 <template>
   <v-dialog v-model="showDialog" max-width="500">
     <template v-slot:activator="{ on, attrs }">
-      <v-btn @click="deleteCommentRequested" icon :attrs="attrs" v-on="on">
+      <v-btn icon :attrs="attrs" v-on="on">
         <v-icon small color="primary">mdi-image-album</v-icon>
       </v-btn>
     </template>
     <v-card>
       <v-card-title>Confirm</v-card-title>
       <v-card-text>Please confirm this meme selection</v-card-text>
+      <v-select v-model="creationDate" class="mx-6" outlined
+        label="Select the year of this meme"
+        :items="dates"
+        item-text="text"
+        item-value="value"
+      />
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn color="secondary" @click="showDialog = false">Cancel</v-btn>
@@ -17,6 +23,17 @@
   </v-dialog>
 </template>
 <script>
+import { DateTime } from 'luxon'
+const NOW = DateTime.local()
+const DATES = [
+  { text: 'Now', value: NOW },
+  { text: 'A week ago', value: NOW.minus({ weeks: 1 }) },
+  { text: 'A month ago', value: NOW.minus({ months: 1 }) },
+  { text: 'A year ago', value: NOW.minus({ years: 1 }) },
+  { text: '2 years ago', value: NOW.minus({ years: 2 }) },
+  { text: '3 years ago', value: NOW.minus({ years: 3 }) },
+  { text: '5 years ago', value: NOW.minus({ years: 5 }) }
+]
 export default {
   props: {
     commentId: {
@@ -26,12 +43,17 @@ export default {
   },
   data() {
     return {
-      showDialog: false
+      showDialog: false,
+      dates: DATES,
+      creationDate: NOW
     }
   },
   methods: {
     postMeme() {
-      const payload = { commentId: this.commentId }
+      const payload = {
+        commentId: this.commentId,
+        creationDate: parseInt(this.creationDate.toMillis() / 1e3)
+      }
       this.$axios.$post('/api/meme', payload)
         .then(data => {
           console.log('post meme response: ', data)
