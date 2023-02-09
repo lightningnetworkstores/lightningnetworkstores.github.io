c97f7ea3bcd1d8878c96d0d73d327f405b763d1b
Pedro FR <pedro.fr.22@gmail.com>
2023-02-09 22:10:11 +0100

    Fix headers.js

--- nuxt/server-middleware/headers.js
+++ nuxt/server-middleware/headers.js
@@ -1,13 +1,15 @@
 export default function(req, res, next) {
   const pages = {
-    home: /\/$|\/tag.*$/g,
-    discuss: /discuss/g
+    home: /^\/$|\/tag\/.*$/g, // no path or /tag/*
+    discuss: /\/discuss\/{0,1}$/g
   }
   if(req.url.match(pages.home)) {
-    res.setHeader('Cache-Tag', 'home')
+    res.setHeader('Cache-Tag', 'homepage')
+    res.setHeader('Cache-Control', 'max-age=2')
   }
   if (req.url.match(pages.discuss)) {
     res.setHeader('Cache-Tag', 'discuss')
+    res.setHeader('Cache-Control', 'max-age=2')
   }
   next()
 }
