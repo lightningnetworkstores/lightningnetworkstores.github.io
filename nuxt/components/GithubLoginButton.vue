23a184340fcbd16e8ee41974237ed046cd81ad37
emeceve <emeceve@outlook.com>
2023-02-26 09:56:48 -0300

    Adds Github login button

---
+++ nuxt/components/GithubLoginButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn color="black white--text" dark block @click="handleLoginClick">
+    <v-icon color="white" class="mr-3">
+      mdi-github
+    </v-icon>
+    LOGIN WITH GITHUB
+  </v-btn>
+</template>
+<script>
+export default {
+  methods: {
+    handleLoginClick() {
+      this.$axios
+        .get(`/api/oauthlogin?platform=github&source_page=${encodeURIComponent(this.$route.path)}`)
+        .then((res) => res.data)
+        .then((data) => {
+          const { authorization_url } = data.data
+          window.location.replace(authorization_url)
+        })
+    },
+  },
+}
+</script>
