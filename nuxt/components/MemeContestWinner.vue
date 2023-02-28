9d2ecb53396aeac7e508dc1e355d4d59a24f6bab
Nelson Pérez <bilthon@gmail.com>
2023-02-28 16:52:20 -0500

    feature: displays the bet & vote summary also for the winner meme

--- nuxt/components/MemeContestWinner.vue
+++ nuxt/components/MemeContestWinner.vue
@@ -10,11 +10,23 @@
       <discussion-image
         :url="winner.link"
       />
+      <div class="text-subtitle-1 text-center">
+        <span>Payout: +0%</span>
+        <contest-result-dialog :contest="contest" />
+      </div>
+      <div class="px-5 py-2">
+        <v-divider class="mb-0" />
+        <div class="py-0 text-left" style="min-height: 73px">
+          <span class="text-caption text--secondary">Voted by</span>
+          <vote-summary :votes="votes"/>
+        </div>
+        <v-divider class="mb-0" />
+        <div class="py-0 text-left" style="min-height: 73px">
+          <span class="text-caption text--secondary">Bets</span>
+          <bet-summary :bets="bets"/>
+        </div>
+      </div>
     </v-card>
-    <div class="text-subtitle-1 text-center">
-      <span>Payout: +0%</span>
-      <contest-result-dialog :contest="contest" />
-    </div>
   </div>
 </template>
 <script>
